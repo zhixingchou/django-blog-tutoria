45fffc7bbe67027f3ca08993fdaf6564fd7bb508
root <zhou zhixing>
2017-12-14 15:16:01 +0800

    commit four

--- blogproject/wsgi.py
+++ blogproject/wsgi.py
@@ -11,8 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogproject.settings")
 from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogproject.settings")
 
 
 application = get_wsgi_application()
