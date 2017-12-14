d5a424c3ea0067e39f95a429fec8357c70a3a10a
root <zhou zhixing>
2017-12-14 15:33:32 +0800

    commit five

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'i3k%m-808v3_)^h7975iw4v&fl5chq41^19j@u+b*vx7dvw*q$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
